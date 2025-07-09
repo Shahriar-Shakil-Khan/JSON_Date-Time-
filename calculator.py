58b36420dc8c9d452a98783fa1c11c30f8c281ea
shahriar-shakil01797 <shahriarshakil28@gmail.com>
2025-07-09 13:27:21 +0600

    shakil

---
+++ calculator.py
@@ -0,0 +1,15 @@
+import calculator_module
+
+print(calculator_module.add(10,5))
+
+import calculator_module
+
+print(calculator_module.subtract(10,5))
+
+import calculator_module
+
+print(calculator_module.multiply(10,5))
+
+import calculator_module
+
+print(calculator_module.divide(10,5))
---
+++ calculator1.py
@@ -0,0 +1,3 @@
+from calculator_module import add
+
+print(add(10,5))
